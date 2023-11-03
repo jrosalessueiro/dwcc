e8e5bb2d277cb17f76327e0117a04979842e17d2
joserosalessueiro <jrsueiro@yahoo.es>
2023-11-03 17:53:45 +0100

    Tarea Tabla del 9 finalizada

--- UD2/Actividad 1/Fibonacci.js
+++ UD2/Actividad 1/Fibonacci.js
@@ -1,16 +1,14 @@
-
-let i = 0;
-let j = 1;
-let k = 3;
+"use strict";
+//La sucesión de Fibonacci se caracteriza porque el primer número es 0, el segundo es 1 y
+// a partir de ahí cada elemento es la suma de los dos anteriores.
+var i = 0;
+var j = 1;
+var k = 3;
 console.log("SERIE DE FIBONACCI" + "\n");
-
 console.log("El elemento 1 de la serie de Fibonacci es: 0");
 console.log("El elemento 2 de la serie de Fibonacci es: 1");
-
-while( k <= 10 ) {
-    
-    let resultado = i + j;
-
+while (k <= 10) {
+    var resultado = i + j;
     console.log("El elemento " + k + " de la serie de Fibonacci es: " + resultado);
     k++;
     i = j;
--- UD2/Actividad 1/Tabla9.ts
+++ UD2/Actividad 1/Tabla9.ts
@@ -1,7 +1,22 @@
 //Este programa va a mostrar la tabla del 9
 
-console.log("TABLA del 9.");
+const Tabla9 = (event: Event) => {
+    event.preventDefault();
+    let numero: number = 9;
 
-for (var i: number = 1; i <= 10; i++) {
-    console.log("9 x " + i + " = " + 9 * i);
+    let tabla = document.getElementById('tabla');
+
+    if (tabla != null) {
+        let tablaMultiplicar: string = '<h2>Tabla del 9</h2>';
+        tablaMultiplicar += '<ul>';
+
+        for (let i: number = 1; i <= 10; i++) {
+            tablaMultiplicar += `<li> ${numero} x ${i} = ${numero * i} </li>`;
+        }
+
+        tablaMultiplicar += '</ul>';
+        tabla.innerHTML = tablaMultiplicar;
+    } else {
+        alert("Elemento 'tabla' no encontrado en el documento.");
+    }
 }
