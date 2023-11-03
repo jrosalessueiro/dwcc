9fa7cd24f94dbec2f94aa49931257b6dfb328e56
joserosalessueiro <jrsueiro@yahoo.es>
2023-11-03 19:03:12 +0100

    Tarea Fibonacci completa

--- UD2/Actividad 1/Fibonacci.js
+++ UD2/Actividad 1/Fibonacci.js
@@ -1,16 +1,25 @@
-"use strict";
+
 //La sucesión de Fibonacci se caracteriza porque el primer número es 0, el segundo es 1 y
 // a partir de ahí cada elemento es la suma de los dos anteriores.
-var i = 0;
-var j = 1;
-var k = 3;
-console.log("SERIE DE FIBONACCI" + "\n");
-console.log("El elemento 1 de la serie de Fibonacci es: 0");
-console.log("El elemento 2 de la serie de Fibonacci es: 1");
-while (k <= 10) {
-    var resultado = i + j;
-    console.log("El elemento " + k + " de la serie de Fibonacci es: " + resultado);
-    k++;
-    i = j;
-    j = resultado;
+
+const Fibonacci = (event) => {
+    event.preventDefault();
+    let i = 0;
+    let j = 1;
+    let k = 3;
+
+    let sucesion = document.getElementById('sucesion');
+    let SucecionFibonacci = '<h2>Suceción de Fibonacci</h2>';
+    SucecionFibonacci += '<ul>';
+    SucecionFibonacci += `<li>El elemento 1 de la serie de Fibonacci es: 0</li>`;
+    SucecionFibonacci += `<li>El elemento 2 de la serie de Fibonacci es: 1</li>`;
+    while (k <= 10) {
+        let resultado = i + j;
+        SucecionFibonacci += `<li>El elemento  ${k}  de la serie de Fibonacci es:  ${resultado}</li>`;
+        k++;
+        i = j;
+        j = resultado;
+    }
+    SucecionFibonacci += '</ul>';
+    sucesion.innerHTML = SucecionFibonacci;
 }
--- UD2/Actividad 1/Fibonacci.ts
+++ UD2/Actividad 1/Fibonacci.ts
@@ -1,22 +1,29 @@
 //La sucesión de Fibonacci se caracteriza porque el primer número es 0, el segundo es 1 y
 // a partir de ahí cada elemento es la suma de los dos anteriores.
 
+const Fibonacci = (event: Event) => {
+    event.preventDefault();
+    let i: number = 0;
+    let j: number = 1;
+    let k: number = 3;
 
-var i: number = 0;
-var j: number = 1;
-var k: number = 3;
+    let sucesion = document.getElementById('sucesion');
+    if (sucesion != null) {
 
-console.log("SERIE DE FIBONACCI" + "\n");
-
-console.log("El elemento 1 de la serie de Fibonacci es: 0");
-console.log("El elemento 2 de la serie de Fibonacci es: 1");
-
-while (k <= 10) {
-
-    var resultado: number = i + j;
-
-    console.log("El elemento " + k + " de la serie de Fibonacci es: " + resultado);
-    k++;
-    i = j;
-    j = resultado;
+        let SucecionFibonacci: string = '<h2>Suceción de Fibonacci</h2>';
+        SucecionFibonacci += '<ul>';
+        SucecionFibonacci += `<li>El elemento 1 de la serie de Fibonacci es: 0</li>`;
+        SucecionFibonacci += `<li>El elemento 2 de la serie de Fibonacci es: 1</li>`;
+        while (k <= 10) {
+            let resultado: number = i + j;
+            SucecionFibonacci += `<li>El elemento  ${k}  de la serie de Fibonacci es:  ${resultado}</li>`;
+            k++;
+            i = j;
+            j = resultado;
+        }
+        SucecionFibonacci += '</ul>';
+        sucesion.innerHTML = SucecionFibonacci;
+    } else {
+        alert("Elemento 'sucesion' no encontrado en el documento.");
+    }
 }
