8932aa0e7eb991f6ef0e8ec6eee21d999cd54ec0
joserosalessueiro <jrsueiro@yahoo.es>
2023-11-04 11:47:09 +0100

    Actividad 2 Etapa de la vida con bucle if

---
+++ UD2/Actividad 2/Edad.js
@@ -0,0 +1,23 @@
+const Etapa = (numero) => {
+    let etapa = "";
+
+    if (numero >= 0 && numero <= 10) {
+        etapa = "Niño/a";
+    } else if (numero <= 18) {
+        etapa = "Adolescente";
+
+    } else if (numero <= 30) {
+        etapa = "Joven";
+
+    } else if (numero <= 64) {
+        etapa = "Adulto";
+
+    } else if (numero <= 100) {
+        etapa = "Jubilado/a";
+
+    } else {
+        etapa = "El valor de la edad no es válido ya que debe ser entre 0 y 100 años."
+    }
+
+    return etapa;
+}
